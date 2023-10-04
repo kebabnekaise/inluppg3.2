6b520a58f55a26c20072d8b4a560ab333309dfd1
emsi06001 <emsi06001@E-PW0234N3>
2023-10-04 12:25:56 +0200

    Add project files.

---
+++ inluppg3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace inluppg3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? Svara j för ja och n för nej");
+            char ansG = char.Parse(Console.ReadLine());
+            Console.WriteLine("Hur gammal är du?");
+            int ansA = int.Parse(Console.ReadLine());
+
+            if (ansG == 'j' && ansA < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu :(");
+            }
+        }
+    }
+}
